b7e32d6759a96b9f95d851f7930a363864e49802
dldmsql <yhy5913@gmail.com>
2023-02-20 16:19:20 +0900

    feat: #6 컨틀롤러 함수 정의 및 공통 엔티티 수정

--- src/main/kotlin/com/example/jhouse_server/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/post/controller/PostController.kt
@@ -1,11 +1,56 @@
 package com.example.jhouse_server.domain.post.controller
 
+import com.example.jhouse_server.domain.post.dto.PostCreateReqDto
+import com.example.jhouse_server.domain.post.dto.PostResDto
+import com.example.jhouse_server.domain.post.dto.PostUpdateReqDto
 import com.example.jhouse_server.domain.post.service.PostService
+import com.example.jhouse_server.global.response.ApplicationResponse
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1/posts")
 class PostController(
         val postService: PostService
 ) {
+    @PostMapping
+    fun createPost(
+            @RequestBody req : PostCreateReqDto
+    ) : ApplicationResponse<PostResDto> {
+        return ApplicationResponse.ok(postService.createPost(req))
+    }
 
+    @GetMapping
+    fun getPostAll() : ApplicationResponse<List<PostResDto>> {
+        return ApplicationResponse.ok(postService.getPostAll())
+    }
+
+    @GetMapping("/{postId}")
+    fun getPostOne(
+        @PathVariable postId: Long
+    ) : ApplicationResponse<PostResDto> {
+        return ApplicationResponse.ok(postService.getPostOne(postId))
+    }
+
+    @PutMapping("/{postId}")
+    fun updatePost(
+        @PathVariable postId: Long,
+        @RequestBody req : PostUpdateReqDto
+    ) : ApplicationResponse<PostResDto> {
+        return ApplicationResponse.ok(postService.updatePost(postId, req))
+    }
+
+    @DeleteMapping("/{postId}")
+    fun deletePost(
+        @PathVariable postId: Long
+    ) : ApplicationResponse<Nothing> {
+        postService.deletePost(postId)
+        return ApplicationResponse.ok()
+    }
 }
--- src/main/kotlin/com/example/jhouse_server/domain/post/entity/PostImageUrlsConverter.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/post/entity/PostImageUrlsConverter.kt
@@ -1,4 +1,4 @@
-package com.example.jhouse_server.domain.post.entity
+package com.example.jhouse_server.domain.post.entity // ktlint-disable package-name
 
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
@@ -7,7 +7,7 @@ import javax.persistence.Converter
 class PostImageUrlsConverter : AttributeConverter<List<String>, String?> {
     private val splitStr = ","
     override fun convertToDatabaseColumn(attribute: List<String>): String? {
-        return if(attribute.isNotEmpty()) {
+        return if (attribute.isNotEmpty()) {
             attribute.joinToString(splitStr)
         } else {
             null
