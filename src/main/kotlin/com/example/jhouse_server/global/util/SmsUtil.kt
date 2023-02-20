b59a47de9ba8a319b74aaa30d47acc5cb5d7b0c8
MoonMinHyuk1 <moon981018@gmail.com>
2023-02-21 04:14:56 +0900

    feat: sms 기능 추가

--- src/main/kotlin/com/example/jhouse_server/global/util/SmsUtil.kt
+++ src/main/kotlin/com/example/jhouse_server/global/util/SmsUtil.kt
@@ -1,4 +1,35 @@
 package com.example.jhouse_server.global.util
 
-class SmsUtil {
+import net.nurigo.java_sdk.api.Message
+import net.nurigo.java_sdk.exceptions.CoolsmsException
+import org.json.simple.JSONObject
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class SmsUtil (
+        @Value("\${cool.sms.api-key}") val key: String,
+        @Value("\${cool.sms.api-secret}") val secret: String,
+        @Value("\${cool.sms.from}") val from: String
+) {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    fun sendMessage(phoneNum: String, code: String) {
+        val message = Message(key, secret)
+        var params: HashMap<String, String> = HashMap()
+
+        params["to"] = phoneNum
+        params["from"] = from
+        params["type"] = "SMS"
+        params["text"] = "[주말의집 인증번호] 본인확인 인증번호($code)를 입력해주세요."
+
+        try {
+            val obj: JSONObject = message.send(params)
+            log.info(obj.toString())
+        } catch (e: CoolsmsException) {
+            throw RuntimeException("문자 전송에 실패했습니다.")
+        }
+    }
 }
