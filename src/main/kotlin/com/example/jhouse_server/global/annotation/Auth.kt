64c2ac8cd933c8ae272151750c9576128dfd09da
MoonMinHyuk1 <moon981018@gmail.com>
2023-02-21 04:15:53 +0900

    feat: Auth 어노테이션 추가

--- src/main/kotlin/com/example/jhouse_server/global/annotation/Auth.kt
+++ src/main/kotlin/com/example/jhouse_server/global/annotation/Auth.kt
@@ -1,3 +1,5 @@
 package com.example.jhouse_server.global.annotation
 
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Auth()
--- src/main/kotlin/com/example/jhouse_server/global/config/WebConfig.kt
+++ src/main/kotlin/com/example/jhouse_server/global/config/WebConfig.kt
@@ -1,4 +1,16 @@
 package com.example.jhouse_server.global.config
 
-class WebConfig {
+import com.example.jhouse_server.global.resolver.AuthResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig (
+        val authResolver: AuthResolver
+): WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(authResolver)
+    }
 }
