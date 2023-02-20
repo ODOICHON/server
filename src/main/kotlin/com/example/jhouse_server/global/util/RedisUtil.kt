0d6a1899d7a68ee8324e11486143974fe8f80be7
MoonMinHyuk1 <moon981018@gmail.com>
2023-02-21 04:14:33 +0900

    feat: redis 기능 추가

--- src/main/kotlin/com/example/jhouse_server/global/util/RedisUtil.kt
+++ src/main/kotlin/com/example/jhouse_server/global/util/RedisUtil.kt
@@ -1,4 +1,25 @@
 package com.example.jhouse_server.global.util
 
-class RedisUtil {
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ValueOperations
+import org.springframework.stereotype.Component
+
+@Component
+class RedisUtil (
+        val redisTemplate: RedisTemplate<String, String>
+) {
+
+    fun setValues(key: String, value: String) {
+        val values: ValueOperations<String, String> = redisTemplate.opsForValue()
+        values.set(key, value)
+    }
+
+    fun getValues(key: String): String? {
+        val values: ValueOperations<String, String> = redisTemplate.opsForValue()
+        return values.get(key)
+    }
+
+    fun deleteValues(key: String) {
+        redisTemplate.delete(key)
+    }
 }
