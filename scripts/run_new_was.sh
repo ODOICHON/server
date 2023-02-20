110a50df23fc042859f5a4e9bb9c3bf1ac0227d6
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-02-20 13:35:52 +0900

    chore: 배포스크립트 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -20,6 +20,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/duaily-server/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/duaily-server/build/libs/jhouse_server-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
