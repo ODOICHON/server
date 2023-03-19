cf79e0d2c89ae7abb45912dec92df28b929c7856
MoonMinHyuk1 <moon981018@gmail.com>
2023-03-19 17:36:40 +0900

    refactor: test 수정

---
+++ src/main/kotlin/com/example/jhouse_server/domain/user/entity/UserJoinPath.kt
@@ -0,0 +1,4 @@
+package com.example.jhouse_server.domain.user.entity
+
+class UserJoinPath {
+}
---
+++ src/main/kotlin/com/example/jhouse_server/domain/user/repository/UserJoinPathRepository.kt
@@ -0,0 +1,4 @@
+package com.example.jhouse_server.domain.user.repository
+
+interface UserJoinPathRepository {
+}
