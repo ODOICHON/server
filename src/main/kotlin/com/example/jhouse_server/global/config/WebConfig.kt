f0024dee2467fcf182e9220d6b3bc72ef98c6420
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-03-20 19:41:26 +0900

    fix: CORS

--- src/main/kotlin/com/example/jhouse_server/global/config/WebConfig.kt
+++ src/main/kotlin/com/example/jhouse_server/global/config/WebConfig.kt
@@ -3,14 +3,22 @@ package com.example.jhouse_server.global.config
 import com.example.jhouse_server.global.resolver.AuthUserResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebConfig (
         val authUserResolver: AuthUserResolver
 ): WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(authUserResolver)
+    }
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
