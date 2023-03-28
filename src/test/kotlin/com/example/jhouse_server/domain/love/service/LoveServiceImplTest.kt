554d571b610ce6809dba8b9274992da324368608
dldmsql <yhy5913@gmail.com>
2023-03-29 01:31:12 +0900

    test: 좋아요 서비스 계층 유닛 테스트 코드 작성

---
+++ src/test/kotlin/com/example/jhouse_server/domain/love/service/LoveServiceImplTest.kt
@@ -0,0 +1,90 @@
+package com.example.jhouse_server.domain.love.service
+
+import com.example.jhouse_server.domain.board.repository.BoardRepository
+import com.example.jhouse_server.domain.board.service.BoardService
+import com.example.jhouse_server.domain.love.entity.Love
+import com.example.jhouse_server.domain.love.repository.LoveRepository
+import com.example.jhouse_server.domain.user.UserSignInReqDto
+import com.example.jhouse_server.domain.user.repository.UserRepository
+import com.example.jhouse_server.domain.user.service.UserService
+import com.example.jhouse_server.global.util.MockEntity
+import com.example.jhouse_server.global.util.findByIdOrThrow
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+internal class LoveServiceImplTest @Autowired constructor(
+    val loveService: LoveService,
+    val loveRepository: LoveRepository,
+    val boardService: BoardService,
+    val boardRepository: BoardRepository,
+    val userService: UserService,
+    val userRepository: UserRepository
+) {
+    private val userSignUpReqDto = MockEntity.testUserSignUpDto()
+    private val testSignUpReqDto = MockEntity.testUser1()
+    @BeforeEach
+    fun `회원가입`() {
+        // default user
+        userService.signUp(userSignUpReqDto)
+        // another user
+        userService.signUp(testSignUpReqDto)
+    }
+    fun `로그인`(email: String, password: String) {
+        userService.signIn(UserSignInReqDto(email, password))
+    }
+    fun `게시글_작성`() : Long {
+        val writer = userRepository.findByEmail(testSignUpReqDto.email).get()
+        return boardService.createBoard(MockEntity.boardReqDto(), writer)
+    }
+
+    @Test
+    @DisplayName("게시글 좋아요")
+    fun love_board() {
+        // given
+        로그인(userSignUpReqDto.email, userSignUpReqDto.password)
+        val board = 게시글_작성()
+        val user = userRepository.findByEmail(userSignUpReqDto.email).get()
+        // when
+        val loved = loveService.loveBoard(board, user)
+        // then
+        val findBoard = boardRepository.findByIdOrThrow(board)
+        val findLoved = loveRepository.findByUserAndBoard(user, findBoard)
+        assertThat(findLoved.board).isEqualTo(findBoard)
+    }
+
+    @Test
+    @DisplayName("게시글 좋아요 취소")
+    fun hate_board() {
+        // given
+        로그인(userSignUpReqDto.email, userSignUpReqDto.password)
+        val board = 게시글_작성()
+        val user = userRepository.findByEmail(userSignUpReqDto.email).get()
+        val loved = loveService.loveBoard(board, user)
+        // when
+        loveService.hateBoard(board, user)
+        val findBoard = boardRepository.findById(board)
+        assertThat(findBoard.get().love.filter { it.id == loved }).isEmpty()
+    }
+
+    @Test
+    @DisplayName("게시글 좋아요 여부 확인")
+    fun is_loved_board() {
+        // given
+        로그인(userSignUpReqDto.email, userSignUpReqDto.password)
+        val user = userRepository.findByEmail(userSignUpReqDto.email).get()
+        val board = 게시글_작성()
+        loveService.loveBoard(board, user)
+        // when
+        val isLoved = loveService.isLovedBoard(board, user)
+        // then
+        assertThat(isLoved).isTrue
+    }
+}
