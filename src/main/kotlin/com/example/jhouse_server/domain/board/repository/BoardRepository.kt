9ec46b73853dbad41cce2a06423304c301471eec
dldmsql <yhy5913@gmail.com>
2023-04-13 17:34:41 +0900

    사용하지 않는 함수 제거
    BoardRepository 내 네이티브 쿼리로 작성된 조회 함수 제거

--- src/main/kotlin/com/example/jhouse_server/domain/board/repository/BoardRepository.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/board/repository/BoardRepository.kt
@@ -5,13 +5,7 @@ import com.example.jhouse_server.domain.board.entity.Board
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface BoardRepository: JpaRepository<Board, Long>, BoardRepositoryCustom {
     abstract fun findAllByPrefixCategoryAndUseYn(prefixCategory : PrefixCategory, useYn: Boolean, pageable: Pageable): Page<Board>
-    @Query(
-        nativeQuery = true,
-        value = "select * from board where prefix_category = :prefixCategory",
-    countQuery = "select count(*) from board")
-    fun findAllByPrefixCategory(prefixCategory: String, pageable: Pageable) : Page<Board>
 }
