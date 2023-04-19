a9fdd7372da9d227638f49e840cac939cc9061de
dldmsql <yhy5913@gmail.com>
2023-04-19 12:25:08 +0900

    #59 게시글 검색 API 구현
    게시글 검색 시, 커뮤니티 대분류와 키워드를 통해 검색합니다. 검색 조건으로는 제목, 내용, 작성자 닉네임으로 제한되어 있습니다.

--- src/main/kotlin/com/example/jhouse_server/domain/board/repository/BoardRepositoryCustom.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/board/repository/BoardRepositoryCustom.kt
@@ -1,10 +1,13 @@
 package com.example.jhouse_server.domain.board.repository
 
 import com.example.jhouse_server.admin.board.dto.AdminBoardSearch
+import com.example.jhouse_server.domain.board.PrefixCategory
+import com.example.jhouse_server.domain.board.entity.Board
 import com.example.jhouse_server.domain.board.repository.dto.AdminBoardResult
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 interface BoardRepositoryCustom {
     fun getBoardListWithPaging(adminBoardSearch: AdminBoardSearch, pageable: Pageable): Page<AdminBoardResult>
+    fun getBoardAllWithKeyword(name: PrefixCategory, keyword: String, pageable: Pageable): Page<Board>
 }
