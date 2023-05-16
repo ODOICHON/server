42fbd7295e408a2f57e108784be501d922d54ef4
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-16 23:38:17 +0900

    feat: record review domain

--- src/main/kotlin/com/example/jhouse_server/domain/record_review/entity/RecordReview.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/record_review/entity/RecordReview.kt
@@ -1,6 +1,8 @@
 package com.example.jhouse_server.domain.record_review.entity
 
 import com.example.jhouse_server.domain.record.entity.Record
+import com.example.jhouse_server.domain.record_review.dto.RecordReviewUpdateDto
+import com.example.jhouse_server.domain.record_review_apply.entity.RecordReviewApply
 import com.example.jhouse_server.domain.user.entity.User
 import com.example.jhouse_server.global.entity.BaseEntity
 import javax.persistence.*
@@ -14,6 +16,10 @@ class RecordReview(
     var status: RecordReviewStatus,
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "apply_id", nullable = false)
+    var apply: RecordReviewApply,
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "record_id", nullable = false)
     var record: Record,
 
@@ -25,4 +31,9 @@ class RecordReview(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long = 0L
 ): BaseEntity() {
+
+    fun updateRecordReview(recordReviewUpdateDto: RecordReviewUpdateDto) {
+        this.content = recordReviewUpdateDto.content
+        this.status = RecordReviewStatus.getStatus(recordReviewUpdateDto.status)
+    }
 }
