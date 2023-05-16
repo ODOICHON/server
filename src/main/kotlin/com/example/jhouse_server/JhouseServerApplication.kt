54be0187a08222e855eaf98104e15ce65f309486
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-16 23:34:18 +0900

    feat: scheduler

--- src/main/kotlin/com/example/jhouse_server/JhouseServerApplication.kt
+++ src/main/kotlin/com/example/jhouse_server/JhouseServerApplication.kt
@@ -2,7 +2,9 @@ package com.example.jhouse_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class JhouseServerApplication
 
