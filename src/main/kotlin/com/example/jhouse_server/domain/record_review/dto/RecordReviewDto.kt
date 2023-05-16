630103cdcdaf50383f3bad8d0dd494da1aa089f0
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-16 23:38:38 +0900

    feat: record review dto

--- src/main/kotlin/com/example/jhouse_server/domain/record_review/dto/RecordReviewDto.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/record_review/dto/RecordReviewDto.kt
@@ -1,3 +1,24 @@
 package com.example.jhouse_server.domain.record_review.dto
 
-data class RecordReviewDto()
+import java.time.LocalDateTime
+
+data class RecordReviewReqDto(
+    //JsonProperty, validation 추가
+    val recordId: Long,
+    val content: String,
+    val status: String
+)
+
+data class RecordReviewUpdateDto(
+    //JsonProperty, validation 추가
+    val content: String,
+    val status: String
+)
+
+data class RecordReviewResDto(
+    val recordId: Long,
+    val content: String,
+    val status: String,
+    val nickName: String,
+    val createAt: LocalDateTime
+)
