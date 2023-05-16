7bc8493fa89e2dd7d70249db7af87002d5e72b32
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-16 23:36:18 +0900

    feat: record review apply dto

--- src/main/kotlin/com/example/jhouse_server/domain/record_review_apply/dto/RecordReviewApplyDto.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/record_review_apply/dto/RecordReviewApplyDto.kt
@@ -1,3 +1,6 @@
 package com.example.jhouse_server.domain.record_review_apply.dto
 
-data class RecordReviewApplyDto()
+data class RecordReviewApplyResDto(
+    val status: String,
+    val nickName: String
+)
