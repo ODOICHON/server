0db42f19448f522b2fa3b18f63eba57deaa30e00
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-18 17:47:46 +0900

    restore

--- src/main/kotlin/com/example/jhouse_server/JhouseServerApplication.kt
+++ src/main/kotlin/com/example/jhouse_server/JhouseServerApplication.kt
@@ -2,9 +2,7 @@ package com.example.jhouse_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
 @SpringBootApplication
 class JhouseServerApplication
 
