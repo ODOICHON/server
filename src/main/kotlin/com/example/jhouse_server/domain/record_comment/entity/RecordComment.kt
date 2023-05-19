6e394c158d56ac92ae8efbf8ae8c301a7c800fd3
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-19 16:37:08 +0900

    feat: record comment domain

--- src/main/kotlin/com/example/jhouse_server/domain/record_comment/entity/RecordComment.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/record_comment/entity/RecordComment.kt
@@ -1,6 +1,7 @@
 package com.example.jhouse_server.domain.record_comment.entity
 
 import com.example.jhouse_server.domain.record.entity.Record
+import com.example.jhouse_server.domain.record_comment.dto.RecordCommentUpdateDto
 import com.example.jhouse_server.domain.user.entity.User
 import com.example.jhouse_server.global.entity.BaseEntity
 import javax.persistence.*
@@ -18,8 +19,31 @@ class RecordComment(
     @JoinColumn(name = "user_id", nullable = false)
     var user: User,
 
+    var ref: Long,
+
+    var step: Long,
+
+    var level: Long,
+
+    var allChildrenSize: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    var parent: RecordComment?,
+
+    @OneToMany(mappedBy = "parent")
+    var children: MutableList<RecordComment> = mutableListOf(),
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long = 0L
 ): BaseEntity() {
+
+    fun updateRecordComment(recordCommentUpdateDto: RecordCommentUpdateDto) {
+        this.content = recordCommentUpdateDto.content
+    }
+
+    fun updateAllChildrenSize() {
+        this.allChildrenSize++
+    }
 }
