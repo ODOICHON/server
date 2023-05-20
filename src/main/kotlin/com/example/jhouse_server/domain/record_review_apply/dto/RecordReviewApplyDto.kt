6d62b61cd8d08c0a96bb1236f711a1b9b24f2524
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-20 14:44:04 +0900

    refactor: refactor dto

--- src/main/kotlin/com/example/jhouse_server/domain/record_review_apply/dto/RecordReviewApplyDto.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/record_review_apply/dto/RecordReviewApplyDto.kt
@@ -1,6 +1,9 @@
 package com.example.jhouse_server.domain.record_review_apply.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class RecordReviewApplyResDto(
     val status: String,
+    @JsonProperty("nick_name")
     val nickName: String
 )
