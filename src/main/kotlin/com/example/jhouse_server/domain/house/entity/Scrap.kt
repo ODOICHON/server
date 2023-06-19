8ae70e70680d44c414884d1e131b4913e274474c
dldmsql <yhy5913@gmail.com>
2023-06-19 23:29:50 +0900

    빈집 게시글 엔티티 구성
    기능 명세서에 따른 엔티티 생성 및 변경

---
+++ src/main/kotlin/com/example/jhouse_server/domain/house/entity/Scrap.kt
@@ -0,0 +1,21 @@
+package com.example.jhouse_server.domain.house.entity
+
+import com.example.jhouse_server.domain.house.entity.House
+import com.example.jhouse_server.domain.user.entity.User
+import com.example.jhouse_server.global.entity.BaseEntity
+import javax.persistence.*
+
+@Entity
+class Scrap(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var subscriber: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "house_id")
+    var house : House,
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) : BaseEntity() {
+}
