989e568a684e629ebd35511f4851ef4b7850165a
dldmsql <yhy5913@gmail.com>
2023-06-23 22:03:04 +0900

    공백에 대한 유효성 처리 추가
    더미 데이터 글자수 10502로 세팅

--- src/main/kotlin/com/example/jhouse_server/global/aop/Validator.kt
+++ src/main/kotlin/com/example/jhouse_server/global/aop/Validator.kt
@@ -1,24 +1,30 @@
 package com.example.jhouse_server.global.aop
 
 import com.example.jhouse_server.domain.board.service.getContent
+import com.example.jhouse_server.global.aop.log.logger
 import java.lang.annotation.Documented
 import javax.validation.Constraint
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 import javax.validation.Payload
+import kotlin.math.log
 import kotlin.reflect.KClass
 
 
 class CodeValidator : ConstraintValidator<CodeValid, String>{
     override fun isValid(value: String, context: ConstraintValidatorContext?): Boolean {
-        return getContent(value).length > 10000
+        if (value.isBlank()) {
+            return true
+        }
+        val contents = getContent(value)
+        return contents.length <= 10000
     }
 
 }
 
 @Target(*[AnnotationTarget.FIELD])
 @Retention(AnnotationRetention.RUNTIME)
-@Constraint(validatedBy = arrayOf(CodeValidator::class))
+@Constraint(validatedBy = [CodeValidator::class])
 @Documented
 annotation class CodeValid(
     val message: String = "게시글의 내용이 10,000자를 넘을 수 없습니다.",
