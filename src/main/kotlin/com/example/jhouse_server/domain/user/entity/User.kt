2611857259ae09d22b9e6b09d2bcb015e08d2d54
dldmsql <yhy5913@gmail.com>
2023-09-25 23:15:17 +0900

    fix: 사용자 이메일, 아이디 암복호화 컨버터 해제

--- src/main/kotlin/com/example/jhouse_server/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/jhouse_server/domain/user/entity/User.kt
@@ -19,7 +19,7 @@ import javax.persistence.*
 @DiscriminatorValue("U")
 class User(
 
-    @Column(name = "email", nullable = true)
+    @Column(name = "email제", nullable = true)
     var email: String,
 
     @Column(name = "user_name")
