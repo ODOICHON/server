05c76e837d1dfe65240832c81799b4e07b46d804
dldmsql <yhy5913@gmail.com>
2023-10-28 11:42:16 +0900

    refactor: #272 관리자페이지-사용자파트 주석 추가

--- src/main/kotlin/com/example/jhouse_server/admin/anaylsis/controller/AdminAnalysisController.kt
+++ src/main/kotlin/com/example/jhouse_server/admin/anaylsis/controller/AdminAnalysisController.kt
@@ -10,21 +10,42 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/admin/analysis")
 class AdminAnalysisController (
+    /**
+     * =============================================================================================
+     *  DI for Repository
+     * =============================================================================================
+     * */
     val analysisService: AnalysisService
         ){
-
+    /**
+     * =============================================================================================
+     * 사용자(일반 사용자, 공인중개사 ) 회원 가입 경로 조회
+     *
+     * @author YoonTaeminnnn
+     * @param model
+     * @return analysis/joinPath 회원 가입 경로 페이지
+     * =============================================================================================
+     * */
     @GetMapping("/join-path")
     fun getAnalysisJoinPath(model : Model) : String {
         val result = analysisService.getAnalysisJoinPathResult()
-        model.addAttribute("joinPathResults", result)
+        model.addAttribute("joinPathResults", result) // 가입 경로 결과
         return "analysis/joinPath"
 
     }
-
+    /**
+     * =============================================================================================
+     * 사용자 연령대 조회
+     *
+     * @author YoonTaeminnnn
+     * @param model
+     * @return analysis/age 연령대 페이지
+     * =============================================================================================
+     * */
     @GetMapping("/age")
     fun getAnalysisAge(model : Model) : String {
         val result = analysisService.getAnalysisAgeResult()
-        val label = Age.values().map { it.value }
+        val label = Age.values().map { it.value } // Age enum class value 리스트 추출
         model.addAttribute("rate", result.map { it.rate })
         model.addAttribute("count", result.map { it.count })
         model.addAttribute("label", label)
