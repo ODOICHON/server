85fb2fa37b0b287692e84e3d1f562c583c80da3e
dldmsql <yhy5913@gmail.com>
2023-10-28 12:56:16 +0900

    refactor: #272 관리자 페이지 - 유저 관리 주석 추가 및 불필요한 코드 제거

--- src/main/kotlin/com/example/jhouse_server/admin/user/controller/AdminUserController.kt
+++ src/main/kotlin/com/example/jhouse_server/admin/user/controller/AdminUserController.kt
@@ -1,15 +1,11 @@
 package com.example.jhouse_server.admin.user.controller
 
-import com.example.jhouse_server.admin.user.dto.AdminAgentSearch
-import com.example.jhouse_server.admin.user.dto.AdminJoinAgentList
-import com.example.jhouse_server.admin.user.dto.AgentSearchFilter
-import com.example.jhouse_server.admin.user.dto.AdminUserWithdrawalSearch
-import com.example.jhouse_server.admin.user.dto.AdminWithdrawalList
-import com.example.jhouse_server.admin.user.dto.UserSearchFilter
+import com.example.jhouse_server.admin.user.dto.*
 import com.example.jhouse_server.admin.user.service.AdminUserService
 import com.example.jhouse_server.domain.user.entity.UserType
-import com.example.jhouse_server.domain.user.entity.UserType.*
-import com.example.jhouse_server.domain.user.entity.WithdrawalStatus.*
+import com.example.jhouse_server.domain.user.entity.UserType.AGENT
+import com.example.jhouse_server.domain.user.entity.UserType.NONE
+import com.example.jhouse_server.domain.user.entity.WithdrawalStatus.WAIT
 import com.example.jhouse_server.domain.user.entity.agent.AgentStatus
 import com.example.jhouse_server.domain.user.repository.AgentRepository
 import com.example.jhouse_server.domain.user.repository.UserRepository
@@ -17,38 +13,55 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("/admin/user")
 class AdminUserController(
+    /**
+     * =============================================================================================
+     *  DI for Service
+     * =============================================================================================
+     * */
     val userRepository: UserRepository,
     val agentRepository: AgentRepository,
     val adminUserService: AdminUserService
 ) {
 
-
-    // 공인중계사 가입요청 리스트
+    /**
+     * =============================================================================================
+     *  공인중개사 회원 가입 요청 목록 조회
+     *  @param adminAgentSearch
+     *  @param adminJoinAgentList
+     *  @param model
+     *  @param pageable
+     *  @return user/agentJoin
+     * =============================================================================================
+     * */
     @GetMapping("/join")
     fun getAgentList(@ModelAttribute("searchForm") adminAgentSearch: AdminAgentSearch,
                      @ModelAttribute("joinList") adminJoinAgentList: AdminJoinAgentList,
                      model: Model,
                      @PageableDefault(size=10, page=0) pageable: Pageable): String{
-
+        // 페이징 처리
         val pageCom = pageable.pageNumber / 5
         model.addAttribute("pageCom", pageCom)
         val total = agentRepository.countByStatus(AgentStatus.WAIT)
         model.addAttribute("total", total)
+
+        // 데이터
         model.addAttribute("agentList", userRepository.getWaitingAgentResult(adminAgentSearch, pageable))
         model.addAttribute("filterList", AgentSearchFilter.values())
         return "user/agentJoin"
 
     }
-
+    /**
+     * =============================================================================================
+     *  공인중개사 회원 가입 승인
+     *  @param adminJoinAgentList
+     *  @return redirect:/admin/user/join
+     * =============================================================================================
+     * */
     @PostMapping("/join")
     fun agentJoin(@ModelAttribute("joinList") adminJoinAgentList: AdminJoinAgentList): String {
         adminUserService.agentJoin(adminJoinAgentList)
@@ -62,8 +75,16 @@ class AdminUserController(
         model.addAttribute("total", total)
         model.addAttribute("filterList", filterList)
     }
-
-    // 탈퇴요청 공인중개사 리스트
+    /**
+     * =============================================================================================
+     *  탈퇴한 공인중개사 목록 조회
+     *  @param adminAgentSearch
+     *  @param adminWithdrawalList
+     *  @param model
+     *  @param pageable
+     *  @return user/agentWithdrawal
+     * =============================================================================================
+     * */
     @GetMapping("/withdrawal/agent")
     fun getDeleteReqAgentList(@ModelAttribute("searchForm") adminAgentSearch: AdminAgentSearch
                               , @ModelAttribute("withdrawalList") adminWithdrawalList: AdminWithdrawalList
@@ -73,8 +94,16 @@ class AdminUserController(
         model.addAttribute("agentList", userRepository.getAgentWithdrawalReqResult(adminAgentSearch, pageable))
         return "user/agentWithdrawal"
     }
-
-    // 탈퇴요청 일반회원 리스트
+    /**
+     * =============================================================================================
+     *  탈퇴한 일반 사용자 목록 조회
+     *  @param adminUserWithdrawalSearch
+     *  @param adminWithdrawalList
+     *  @param model
+     *  @param pageable
+     *  @return user/userWithdrawal
+     * =============================================================================================
+     * */
     @GetMapping("/withdrawal")
     fun getDeleteReqUserList(@ModelAttribute("searchForm") adminUserWithdrawalSearch: AdminUserWithdrawalSearch
                              , @ModelAttribute("withdrawalList") adminWithdrawalList: AdminWithdrawalList
@@ -84,18 +113,36 @@ class AdminUserController(
         model.addAttribute("userList", userRepository.getUserWithdrawalReqResult(adminUserWithdrawalSearch, pageable))
         return "user/userWithdrawal"
     }
-
+    /**
+     * =============================================================================================
+     *  회원 탈퇴 처리
+     *  @param type
+     *  @param adminWithdrawalList
+     *  @return redirect:/admin/user/withdrawal || redirect:/admin/user/withdrawal/agent
+     * =============================================================================================
+     * */
     @PostMapping("/withdrawal/{type}")
-    fun withdrawalAgent(@PathVariable("type") type: String,@ModelAttribute("withdrawalList") adminWithdrawalList: AdminWithdrawalList): String{
+    fun withdrawalAgent(
+        @PathVariable("type") type: String,
+        @ModelAttribute("withdrawalList") adminWithdrawalList: AdminWithdrawalList): String{
         adminUserService.withdrawalUser(adminWithdrawalList)
         if (type == "agent") {
             return "redirect:/admin/user/withdrawal/agent"
         }
         return "redirect:/admin/user/withdrawal"
     }
-
+    /**
+     * =============================================================================================
+     *  회원 탈퇴 사용자 정보 상세 조회
+     *  @param id
+     *  @param model
+     *  @return user/withdrawalDetail
+     * =============================================================================================
+     * */
     @GetMapping("/withdrawal/reason/{id}")
-    fun withdrawalDetail(@PathVariable id: Long, model: Model): String {
+    fun withdrawalDetail(
+        @PathVariable id: Long,
+        model: Model): String {
         val findUser = userRepository.findById(id).get()
         model.addAttribute("user", findUser)
 
