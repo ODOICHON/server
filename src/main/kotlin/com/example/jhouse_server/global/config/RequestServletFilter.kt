8fdbc971e4e0e9acf979c51b6b32bb7157bd9732
dldmsql <yhy5913@gmail.com>
2024-02-05 23:49:21 +0900

    feat: 관리자 - 유저 정보 조회 페이지 개발

--- src/main/kotlin/com/example/jhouse_server/global/config/RequestServletFilter.kt
+++ src/main/kotlin/com/example/jhouse_server/global/config/RequestServletFilter.kt
@@ -1,22 +1,22 @@
-package com.example.jhouse_server.global.config
-
-import org.springframework.stereotype.Component
-import javax.servlet.Filter
-import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
-import javax.servlet.http.HttpServletRequest
-
-@Component
-class RequestServletFilter : Filter {
-    override fun doFilter(
-        request: ServletRequest,
-        response: ServletResponse,
-        chain: FilterChain
-    ) {
-        val wrappedRequest: HttpServletRequest =
-            RequestServletWrapper(request as HttpServletRequest)
-
-        chain.doFilter(wrappedRequest, response)
-    }
-}
+//package com.example.jhouse_server.global.config
+//
+//import org.springframework.stereotype.Component
+//import javax.servlet.Filter
+//import javax.servlet.FilterChain
+//import javax.servlet.ServletRequest
+//import javax.servlet.ServletResponse
+//import javax.servlet.http.HttpServletRequest
+//
+//@Component
+//class RequestServletFilter : Filter {
+//    override fun doFilter(
+//        request: ServletRequest,
+//        response: ServletResponse,
+//        chain: FilterChain
+//    ) {
+//        val wrappedRequest: HttpServletRequest =
+//            RequestServletWrapper(request as HttpServletRequest)
+//
+//        chain.doFilter(wrappedRequest, response)
+//    }
+//}
