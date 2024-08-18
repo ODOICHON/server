4d9460c81a464b817cc0aac9975c1546501bf1f9
dldmsql <yhy5913@gmail.com>
2024-08-18 19:40:23 +0900

    feat: 농가거래 파트로 기능 전환

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     // Database
     runtimeOnly("com.mysql:mysql-connector-j")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation ("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+//    implementation ("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
     // Jwt
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
